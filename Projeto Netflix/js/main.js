53d94b72e805aef9f75a420c7a7e6fd4596c9057
carvalhobruno20 <carvalho.bruno20@gmail.com>
2021-04-14 22:46:47 -0300

    Melhorando o layout

--- Projeto Netflix/js/main.js
+++ Projeto Netflix/js/main.js
@@ -1,8 +1,12 @@
 $('.owl-carousel').owlCarousel({
+    dots: false,
     stagePadding: 50,
     loop: true,
     margin: 10,
     nav: false,
+    autoplay: true,
+    autoplayTimeout: 4500,
+    autoplayHoverPause: true,
     responsive: {
         0: {
             items: 1
